b43c6ba107d1e96f37e6b6e0e70c60987fa8846f
pojiro <pojiro.jp@gmail.com>
2023-01-22 12:08:23 +0900

    Add codes to p.56

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -2,10 +2,16 @@
 
 static uint16_t *ledAddress;
 
+static uint16_t convertLedNumberToBit(int ledNumber) {
+  return 1 << (ledNumber - 1);
+}
+
 void LedDriver_Create(uint16_t *address) {
   ledAddress = address;
   *ledAddress = 0;
 }
 
-void LedDriver_TurnOn(int ledNumber) { *ledAddress = 1; }
+void LedDriver_TurnOn(int ledNumber) {
+  *ledAddress |= convertLedNumberToBit(ledNumber);
+}
 void LedDriver_TurnOff(int ledNumber) { *ledAddress = 0; }
